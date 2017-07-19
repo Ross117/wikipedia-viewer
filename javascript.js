5d8f02299c65ea2faaa1ca1b0ac75f7e16a0d73d
Ross117 <RossMorran@hotmail.co.uk>
2017-07-19 20:50:47 +0100

    Add user search functionality and basic design improvements

--- javascript.js
+++ javascript.js
@@ -5,18 +5,21 @@ $(".randomArticle").on("click", () => {
   window.open(link);
 });
 
-// need user entry validation
+
+// request search results from the MediaWiki API based on user input
 $(".articleSearch").on("click", () => {
   "use strict";
 
+  // need user entry validation?
   const $userInput = $(".userInput").val();
-  const $searchResults = $(".searchResults");
 
   $.ajax({
     // use a proxy server to prevent CORS error
-    url: "https://thingproxy.freeboard.io/fetch/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=france",
+    url: "https://thingproxy.freeboard.io/fetch/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + $userInput,
     success: (json) => {
+      "use strict";
 
+      const $searchResults = $(".searchResults");
       //an array of titles returned by the API
       const titles = json[1];
       //an array of descriptions returned by the API
@@ -24,17 +27,30 @@ $(".articleSearch").on("click", () => {
       //an array of links returned by the API
       const links = json[3];
 
+      //clear any existing search results on the page
+      clearSearchResults();
+
       // append the titles, links and descriptions of the search results to the page
       links.map((val, index) => {
-        $searchResults.append("<div><h5><a href='" + val + "' >" + titles[index]
-        + "</a></h5><p>" + description[index] + "</p></div><br>");
+        $searchResults.append("<div><h5 class='text-center'><a href='" + val + "' >" + titles[index] + "</a></h5><p class='text-center'>" + description[index] + "</p></div><br>");
       });
 
     },
     error: () => {
+      "use strict";
 
-      $searchResults.append("Error");
+      const $searchResults = $(".searchResults");
+      clearSearchResults();
+      $searchResults.append("<p>Error</p>");
     }
   });
 
 });
+
+//clear any existing search results on the page
+function clearSearchResults () {
+  "use strict";
+
+  const $searchResults = $(".searchResults");
+  $(".searchResults").html("");
+}
