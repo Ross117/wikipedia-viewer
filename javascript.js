db34e6989cc9550ae95ad9cea9d59573364fe51b
Ross117 <RossMorran@hotmail.co.uk>
2017-07-19 17:21:32 +0100

    First commit, base design

---
+++ javascript.js
@@ -0,0 +1,14 @@
+$(".randomArticle").on("click", () => {
+  "use strict";
+
+  const link = "https://en.wikipedia.org/wiki/Special:Random";
+  window.open(link);
+});
+
+// need user entry validation
+$(".articleSearch").on("click", () => {
+  "use strict";
+
+  const $userInput = $(".userInput");
+
+});
