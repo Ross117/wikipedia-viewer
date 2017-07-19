c35043d392eef75778741d048cd3deda5e20b126
Ross117 <RossMorran@hotmail.co.uk>
2017-07-19 19:41:02 +0100

    Add basic search functionality

--- javascript.js
+++ javascript.js
@@ -9,6 +9,32 @@ $(".randomArticle").on("click", () => {
 $(".articleSearch").on("click", () => {
   "use strict";
 
-  const $userInput = $(".userInput");
+  const $userInput = $(".userInput").val();
+  const $searchResults = $(".searchResults");
+
+  $.ajax({
+    // use a proxy server to prevent CORS error
+    url: "https://thingproxy.freeboard.io/fetch/https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=france",
+    success: (json) => {
+
+      //an array of titles returned by the API
+      const titles = json[1];
+      //an array of descriptions returned by the API
+      const description = json[2];
+      //an array of links returned by the API
+      const links = json[3];
+
+      // append the titles, links and descriptions of the search results to the page
+      links.map((val, index) => {
+        $searchResults.append("<div><h5><a href='" + val + "' >" + titles[index]
+        + "</a></h5><p>" + description[index] + "</p></div><br>");
+      });
+
+    },
+    error: () => {
+
+      $searchResults.append("Error");
+    }
+  });
 
 });
